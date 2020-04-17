fbb1243a511bab633aa7df679cfcf60e560e218c
Thomas J. Daley, JD <tjd@powerdaley.com>
2020-04-17 10:00:52 -0500

    clear index before re-indexing

--- app/reindex.sh
+++ app/reindex.sh
@@ -3,7 +3,8 @@ codes=( ag al bc bo cp cr ed el es fa hs pe )
 for code in "${codes[@]}"
 do
         echo Indexing $code
-	python3 app.py --index --progress --code $code
+	python3 app.py --delete --index --progress --code $code
 done
 python3 app.py --upload
+sudo service jdbot-restutil restart
 
